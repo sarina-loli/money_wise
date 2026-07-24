21d63ff8ef2bc8e42d8ff37686d3ccf6b02f81f8
sarina-loli <sarina123getu@gmail.com>
2026-07-24 03:35:33 -0700

    commit1

--- config/settings.py
+++ config/settings.py
@@ -124,12 +124,12 @@ USE_TZ = True
 # -------------------------------------------------------------------------
 # STATIC & MEDIA FILES
 # -------------------------------------------------------------------------
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
-MEDIA_URL = 'media/'
+MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
